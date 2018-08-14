d5a5f5bca95502ff54f860e3f5fb31c36ab11f2a
Vahan92 <vahan.kalenteryan@gmail.com>
2018-08-14 08:19:25 +0400

    nearlly finished

--- js/main.js
+++ js/main.js
@@ -1,98 +1,3 @@
-// let MyApp = function() {
-//     let self = this,
-//         max = 100,
-//         min = 4,        
-//         print = document.getElementById("print");
-
-//         init = function (divId) {
-//             forCells = divId;
-//         },
-
-//         getId = function(params) {
-//             return document.getElementById(params);
-//         },
-
-//         printing = function(inpVal) {
-//             if (typeof print !== null) {
-//                 print.textContent = "Number of cells: " + inpVal.value * inpVal.value;
-//             }
-//         },
-
-//         checkingBtn = function() {
-//             if(forCells.firstChild){
-//                 self.getId('resets').disabled = false;
-//                 self.getId('colourises').disabled = false;
-//                 self.getId('draws').disabled = true;
-//             } else {
-//                 self.getId('resets').disabled = true;
-//                 self.getId('colourises').disabled = true;
-//                 self.getId('draws').disabled = false;
-//             }
-//         },
-
-//         draw = function() {
-//             let inputVal = self.getId("num").value;
-//             if (inputVal < min || inputVal > max) {
-//                 alert(`Please enter a number between ${min} and ${max}, or you are going to get this annoying alert notification :)`);
-//             } else {
-//                 var i,
-//                     j,
-//                     newDiv,
-//                     newSpan;
-//                 let fragment = document.createDocumentFragment();
-//                 for (i = 0; i < inputVal; i++) {
-//                     let newCol = document.createElement('div');
-//                     newCol.classList.add("column");
-//                     for (j = 0; j < inputVal; j++) {                        
-//                         newDiv = document.createElement('div');
-//                         newCol.appendChild(newDiv);               
-//                     }
-//                     fragment.appendChild(newCol);
-//                 }            
-
-//                 forCells.appendChild(fragment);
-//                 checkingBtn();
-//             }
-//         },       
-
-//         getColor =  function() {
-//             var r = function () { return Math.floor(Math.random()*256) };
-//             return "rgb(" + r() + "," + r() + "," + r() + ")";
-//         },
-
-//         colourise =  function() {
-//             let toColour =  document.querySelectorAll('.column > div'); 
-//             for (let i = 0; i < toColour.length; i++) {
-//                     setTimeout(function() {
-//                         toColour[i].style.backgroundColor = self.getColor();
-//                         toColour[i].style.transition = "all 1.5s";
-//                     }, 1500 * i);           
-//         }
-//         },
-
-//         reset =  function() {
-//             print.textContent = "";
-//             while (forCells.firstChild) {
-//                 forCells.removeChild(forCells.firstChild);
-//             }
-//             checkingBtn();
-//         }
-
-//     return {
-//         init: init,
-//         printing: printing,
-//         draw: draw,
-//         colourise: colourise,
-//         reset: reset
-//     }
-// }
-
-// var simpleApp;
-
-//     var divId = document.getElementById('forCells');
-//     simpleApp = MyApp(); //Invoke the object (function assigned to the object) and assign to simpleApp
-//     simpleApp.init(divId);
-
 getId = function (params) {
     return document.getElementById(params);
 };
@@ -101,10 +6,10 @@ Array.prototype.indicesOf = function (x) {
     return this.reduce((p, c, i) => c === x ? p.concat(i) : p, []);
 };
 
-let randomWord, arrWord, arr = [], changedArr = [], anotherArr = [], checkingArr = [], mia = [], some = [], elen = [], chars = '', count = 6, warning = 3, winning = 0, wordLength = 0, alphaArray = "abcdefghijklmnopqrstuvwxyz".split(""), userVal = getId("txt"),
+let guessRandomWord, arrOfWords, checkIndexes, arr = [], nestedArrOfWords = [], arrOfSameLength = [], hintArr = [], wordCheckingArr = [], mia = [], some = [], arrForResults = [], chars = '', count = 6, points, warning = 3, winning = 0, guessWordLength = 0, alphabetArray = "abcdefghijklmnopqrstuvwxyz".split(""), userVal = getId("txt"),
 
 print = getId("ammountOfLetters");
-wordsLeft = getId("guess");
+charsLeft = getId("guess");
 printChar = getId("ammountOfChars");
 printletters = getId("letters");
 printWarning = getId("warning");
@@ -114,72 +19,38 @@ printErr = getId("error");
 readFile = function () {
     $.get('words.txt', function (data) {
         arr = data.split('\n');
-        console.log(arr);
+        //console.log(arr);
     });
 }
 
 selectWord = () => {
-    arrWord = arr.join(" ").split(" ");
-    console.log(arrWord);
+    arrOfWords = arr.join(" ").split(" ");
+    console.log("arrOfWords ", arrOfWords);
     chars = '';
-    randomWord = arrWord[Math.floor(Math.random() * arrWord.length - 1)].split('');
-    wordLength = randomWord.length;
-    print.textContent = `I am thinking of a word that is ${wordLength} letters long.`;
-    wordsLeft.textContent = `You have 6 guesses left`;
-    printletters.textContent = `Here are the letters left: ${alphaArray}`;
+    guessRandomWord = arrOfWords[Math.floor(Math.random() * arrOfWords.length - 1)].split('');
+    points = [...new Set(guessRandomWord)].length;
+    guessWordLength = guessRandomWord.length;
+    print.textContent = `I am thinking of a word that is ${guessWordLength} letters long.`;
+    charsLeft.textContent = `You have 6 guesses left`;
+    printletters.textContent = `Here are the letters left: ${alphabetArray}`;
     printWarning.textContent = ``;
-    hint();
-    for (let i = 0; i < wordLength; i++) {
+    //hint();
+    for (let i = 0; i < guessWordLength; i++) {
         chars += "*";
     }
     printChar.textContent = chars;
-    console.log(randomWord);
+    console.log(guessRandomWord);
 }
 
-hint = () => {
-    let j,ok;
-    for (let i = 0; i < arrWord.length; i++) {
-        changedArr.push(arrWord[i].split(''));
-    }
-    for (let i = 0; i < changedArr.length; i++) {
-        if (randomWord.length === changedArr[i].length)
-            anotherArr.push(changedArr[i]);
-    }
-    for (let i = 0; i < anotherArr.length; i++) {
-        for (j = 0; j < anotherArr[0].length; j++) {
-            ok = false;
-            if (anotherArr[i][j] === "*") {
-                continue;
-            } else if (anotherArr[i][j] !== "*") {
-                if (anotherArr[i][j] !== checkingArr[j]) {
-                    break;
-                 }
-                 if (anotherArr[i][j] === checkingArr[j])
-                 {
-                      ok = true;
-                 }
-            }
-        }
-        if(j === anotherArr[0].length-1 && ok === true)
-        {
-            elen.push(anotherArr[j]);
-        }
-
-    }
-    console.log(changedArr);
-    console.log("anotherArr ", anotherArr);
-    console.log("elen ", elen);
-}
-
-checking = () => {
+checkingInputValue = () => {
     let letters = /^[A-Za-z]+$/;
     if (!userVal.value.match(letters)) {
         warning -= 1;
         printWarning.textContent = `Oops! That is not a valid letter. You have ${warning} warnings left.`;
         gameOver();
-    } else if (alphaArray.includes(userVal.value.toLowerCase())) {
+    } else if (alphabetArray.includes(userVal.value.toLowerCase())) {
         getInpVal();
-        console.log("alphaArray ", alphaArray, "userVal.value.toLowerCase()", userVal.value.toLowerCase());
+        console.log("alphabetArray ", alphabetArray, "userVal.value.toLowerCase()", userVal.value.toLowerCase());
         gameOver();
     } else {
         warning -= 1;
@@ -190,15 +61,15 @@ checking = () => {
 
 getInpVal = () => {
     let sum = 0, been = false;
-    for (let i = 0; i < wordLength; i++) {
-        if (userVal.value === randomWord[i]) {
+    for (let i = 0; i < guessWordLength; i++) {
+        if (userVal.value === guessRandomWord[i]) {
             chars = chars.substr(0, i) + userVal.value.toLowerCase() + chars.substr(i + 1);
             printChar.textContent = chars;
             printMessage.textContent = `Good guess`;
             been = true;
             sum = 0;
             winning += 1;
-            console.log(alphaArray);
+            console.log("alphabetArray", alphabetArray);
         } else {
             sum = 1;
         }
@@ -207,30 +78,67 @@ getInpVal = () => {
         count -= sum;
         printMessage.textContent = `Oops! That letter is not in my word.`;
     }
-    if (winning === wordLength) {
-        alert("Congratulations, you won");
+    if (winning === guessWordLength) {
+        alert(`Congratulations, you won. Your score is ${count * points}`);
         location.reload();
     }
-    alphaArray.splice(alphaArray.indexOf(userVal.value.toLowerCase()), 1);
-    printletters.textContent = `Here are the letters left: ${alphaArray}`;
+    alphabetArray.splice(alphabetArray.indexOf(userVal.value.toLowerCase()), 1);
+    printletters.textContent = `Here are the letters left: ${alphabetArray}`;
     been = false;
     userVal.value = '';
-    wordsLeft.textContent = `You have ${count} guesses left`;
+    charsLeft.textContent = `You have ${count} guesses left`;
     printChar.textContent = chars;
-    console.log(`chars `,chars);
-    checkingArr.length = 0;
-    checkingArr.push(chars.split(""));
-    console.log("checkingArr ", checkingArr);
-    hint();
-    let jj = Array.from(checkingArr);
-    console.log("jj ", jj);
-    some = jj.indicesOf("*");    
+    console.log(`chars `, chars);
+    wordCheckingArr.length = 0;
+    wordCheckingArr = chars.split("");
+    console.log("wordCheckingArr ", wordCheckingArr);
+    //console.log("jj ", jj);
+    some = wordCheckingArr.indicesOf("*");
+    getIndexesOfGuessedCharacters();
     console.log("some ", some);
+}
+
+hint = () => {
+    nestedArrOfWords = [];
+    arrOfSameLength = [];
+    hintArr = [];
+    let j, ok;
+    for (let i = 0; i < arrOfWords.length; i++) {
+        nestedArrOfWords.push(arrOfWords[i].split(''));
+    }
+    for (let i = 0; i < nestedArrOfWords.length; i++) {
+        if (guessRandomWord.length === nestedArrOfWords[i].length)
+            arrOfSameLength.push(nestedArrOfWords[i]);
+    }
+    for (let i = 0; i < arrOfSameLength.length; i++) {
+        for (j = 0; j < wordCheckingArr.length; j++) {
+            ok= false;
+           if (wordCheckingArr[j] === "*") {
+               ok = true;
+               continue;
+           } else if (wordCheckingArr[j] !== "*" && wordCheckingArr[j] !== arrOfSameLength[i][j]) {
+               ok=false;
+               break;
+           } else {
+               ok = true;
+           }
+                   
+        }
+        if (j === arrOfSameLength[0].length - 1 && ok === true) {
+            arrForResults.push(arrOfSameLength[j]);
+            console.log("from if ", arrForResults);
+        }
+                
+    }
+         
+    console.log(`nestedArrOfWords`, nestedArrOfWords);
+    console.log("arrOfSameLength ", arrOfSameLength);
+    console.log("arrForResults ", arrForResults);
 }
 
 gameOver = () => {
     if (count < 0 || warning < 0) {
-        alert("Game over, you lost");
+        alert(`Game over, you lost, the word is - ${guessRandomWord.join().replace(/,/g, '')}`);
         location.reload();
     }
 }
